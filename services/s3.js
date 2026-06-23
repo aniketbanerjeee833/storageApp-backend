54464d753362be7776afe000ad885023b62cb939
Aniket Banerjee <aniketbanerjee8334845906@gmail.com>
2026-06-23 17:51:32 +0000

    remove env from tracking

--- services/s3.js
+++ services/s3.js
@@ -8,8 +8,14 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3Client = new S3Client({ profile: "nodejs" });
-
+//const s3Client = new S3Client({ profile: "nodejs" });
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 export const createUploadSignedUrl = async ({ key, contentType }) => {
   const command = new PutObjectCommand({
     Bucket: "aniket-storage-app",
