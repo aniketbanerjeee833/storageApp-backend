e45b8f2d6eedf450bed77004c2be6e1ac1d4272b
Aniket Banerjee <aniketbanerjee8334845906@gmail.com>
2026-06-24 22:27:11 +0530

    changed

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ bashChildProcess.on('error', (error) => {
 })
 });
 app.get("/", (req, res) => {
-  res.json("Hello My  World!");
+  res.json("Hello My Storage World!");
 })
 app.use("/directory", checkAuth, directoryRoutes);
 app.use("/file", checkAuth, fileRoutes);
